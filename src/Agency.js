92d0b582c54b5a1e2dc8a2d4daea57c711d6c0d9
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-01 13:59:08 -0700

    Add method to calculate Agency earning based on year

--- src/Agency.js
+++ src/Agency.js
@@ -10,6 +10,36 @@ class Agency {
     return this.tripsData.filter(trip => trip.status === "pending")
   }
 
+  calculateAgencyEarnings(year) {
+    let approvedTrips = this.tripsData.filter(trip => trip.status === "approved" && trip.date.includes(year));
+
+    let costOfTrips = approvedTrips.map(trip => {
+      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
+      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
+      return {
+        date: trip.date,
+        agencyFees: Math.round(totalCost*.10)
+        // id: trip.id,
+        // travelers: trip.travelers,
+        // duration: trip.duration,
+        // status: trip.status,
+        // suggestedActivities: trip.suggestedActivities,
+        // destination: destinationInfo.destination,
+        // totalTripCost: Math.round(totalCost),
+        // overallCost: Math.round(totalCost*1.1),
+        // destinationImage: destinationInfo.image,
+        // destinationAlt: destinationInfo.alt
+      }
+    })
+    return costOfTrips.reduce((totalEarning, trip) => {
+      return totalEarning += trip.agencyFees;
+    },0)
+  }
+
+  calculateTotalTripCost(costPerPerson, totalTravelers, duration, lodgingCostPerDay) {
+    return (costPerPerson * totalTravelers) + (duration * lodgingCostPerDay)
+  }
+
 }
 
 export default Agency;
