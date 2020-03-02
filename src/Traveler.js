30adc2497f6aaf706e2cce7794d1f7a0c9387296
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-03-02 00:01:12 -0700

    Refactor to remove repeating code to a helper function

--- src/Traveler.js
+++ src/Traveler.js
@@ -9,27 +9,81 @@ class Traveler {
     this.destinationData = destinationData;
   }
 
-  filterTravelerTrips() {
-    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "approved");
+  approvedTrips() {
+    return this.tripsData.filter(trip => trip.userID === this.id && trip.status === "approved");
+  }
+
+  filterPendingTrips() {
+    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "pending");
 
     return userTripsData.map(trip => {
       let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
       let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
 
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
+    })
+  }
+
+  filterPastTrips() {
+    let userTripsData = this.approvedTrips();
+    let currentDay = moment();
+
+    let filterPastTrips = userTripsData.filter(trip => {
+      let endDate = moment(trip.date, 'YYYY/MM/DD').add(trip.duration, 'days').format('YYYY/MM/DD');
+    return moment(currentDay).isAfter(endDate);
+    })
+
+    return filterPastTrips.map(trip => {
+      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
+      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
+
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
+    })
+  }
+
+  filterUpcomingTrips() {
+    let userTripsData = this.approvedTrips();
+    let currentDay = moment();
+
+    let filterUpcomingTrips = userTripsData.filter(trip => {
+      let startDate = moment(trip.date, 'YYYY/MM/DD');
+    return moment(currentDay).isBefore(startDate);
+    })
+
+    return filterUpcomingTrips.map(trip => {
+      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
+      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
+
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
+    })
+  }
+
+  filterPresentTrips() {
+    let userTripsData = this.approvedTrips();
+    let currentDay = moment();
+
+    let filterPresentTrips = userTripsData.filter(trip => {
+      let startDate = moment(trip.date, 'YYYY/MM/DD');
+      let endDate = moment(trip.date, 'YYYY/MM/DD').add(trip.duration, 'days').format('YYYY/MM/DD');
+    return moment(currentDay).isBetween(startDate, endDate);
+    })
+
+    return filterPresentTrips.map(trip => {
+      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
+      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
+
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
+    })
+  }
+
+  filterTravelerTrips() {
+    let userTripsData = this.approvedTrips();
+
+    return userTripsData.map(trip => {
+      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
+      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
+
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
     })
   }
 
@@ -50,20 +104,7 @@ class Traveler {
       let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
       let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
 
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
+      return this.tripSummaryCard(trip, destinationInfo, totalCost);
     })
   }
 
@@ -81,124 +122,22 @@ class Traveler {
     return (costPerPerson * totalTravelers) + (duration * lodgingCostPerDay)
   }
 
-  filterPendingTrips() {
-    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "pending");
-
-    return userTripsData.map(trip => {
-      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
-      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
-
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
-    })
+  tripSummaryCard(tripInfo, destinationInfo, totalCost) {
+    return {
+      id: tripInfo.id,
+      travelers: tripInfo.travelers,
+      date: tripInfo.date,
+      duration: tripInfo.duration,
+      status: tripInfo.status,
+      suggestedActivities: tripInfo.suggestedActivities,
+      destination: destinationInfo.destination,
+      totalTripCost: Math.round(totalCost),
+      agencyFees: Math.round(totalCost*.10),
+      overallCost: Math.round(totalCost*1.1),
+      destinationImage: destinationInfo.image,
+      destinationAlt: destinationInfo.alt
+    }
   }
-
-  filterPastTrips() {
-    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "approved");
-
-    let currentDay = moment();
-    let filterPastTrips = userTripsData.filter(trip => {
-      let endDate = moment(trip.date, 'YYYY/MM/DD').add(trip.duration, 'days').format('YYYY/MM/DD');
-    return moment(currentDay).isAfter(endDate);
-  })
-
-    return filterPastTrips.map(trip => {
-      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
-      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
-
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
-    })
-
-  }
-
-  filterUpcomingTrips() {
-    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "approved");
-
-    let currentDay = moment();
-    let filterUpcomingTrips = userTripsData.filter(trip => {
-      let startDate = moment(trip.date, 'YYYY/MM/DD');
-    return moment(currentDay).isBefore(startDate);
-  })
-
-    return filterUpcomingTrips.map(trip => {
-      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
-      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
-
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
-    })
-
-  }
-
-  filterPresentTrips() {
-    let userTripsData = this.tripsData.filter(trip => trip.userID === this.id && trip.status === "approved");
-
-    let currentDay = moment();
-    let filterPresentTrips = userTripsData.filter(trip => {
-      let startDate = moment(trip.date, 'YYYY/MM/DD');
-      let endDate = moment(trip.date, 'YYYY/MM/DD').add(trip.duration, 'days').format('YYYY/MM/DD');
-    return moment(currentDay).isBetween(startDate, endDate);
-  })
-
-    return filterPresentTrips.map(trip => {
-      let destinationInfo = this.destinationData.find(destinationData => trip.destinationID === destinationData.id);
-      let totalCost = parseInt(this.calculateTotalTripCost(destinationInfo.estimatedFlightCostPerPerson, trip.travelers, trip.duration, destinationInfo.estimatedLodgingCostPerDay));
-
-      return {
-        id: trip.id,
-        travelers: trip.travelers,
-        date: trip.date,
-        duration: trip.duration,
-        status: trip.status,
-        suggestedActivities: trip.suggestedActivities,
-        destination: destinationInfo.destination,
-        totalTripCost: Math.round(totalCost),
-        agencyFees: Math.round(totalCost*.10),
-        overallCost: Math.round(totalCost*1.1),
-        destinationImage: destinationInfo.image,
-        destinationAlt: destinationInfo.alt
-      }
-    })
-
-  }
-
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 var moment = require('moment');
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+import $ from 'jquery';
 import './css/base.scss';
 import travelers from './data/traveler.js';
 import trips from '../src/data/trips.js';
 import destinations from '../src/data/destinations.js';
 
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import Login from './Login';
 import Traveler from './Traveler';
 import Agency from './Agency';
@@ -54,12 +48,12 @@ function populateAgencyInfo(agencyId) {
   return `
     <section class="agent-summary">
       <h2>Agent Summary</h2>
-      <div>Estimated Current Year Earnings:</div>
-      <div>${agency.calculateAgencyEarningsforYear("2020")}</div>
+      <div>Estimated ${moment().format("YYYY")} Earnings:</div>
+      <div>${formatter.format(agency.calculateAgencyEarningsforYear(moment().format("YYYY")))}</div>
     </section>
     <section>
     <div>Pending Requests</div>
-      <section>${pendingRequestSummary}</section>
+      <section class="pending-request">${pendingRequestSummary}</section>
     </section>
     <section>
     <div>Current Travelers</div>
@@ -72,99 +66,33 @@ function populateAgencyInfo(agencyId) {
       <div class="search-results">?</div>
     </section>
     `
-
 }
+
 
 function populateTravelerInfo(userID, travelersData) {
   let travelerInfo = travelersData.find(traveler => traveler.id === userID);
   let traveler = new Traveler(travelerInfo, trips, destinations);
-  // let filterTrips = traveler.filterTravelerTripsbyYear("2020");
-  let filterUpcomingTrips = traveler.filterUpcomingTrips();
-  let filterPastTrips = traveler.filterPastTrips();
-  let filterPendingTrips = traveler.filterPendingTrips();
-  let filterPresentTrip = traveler.filterPresentTrips();
   $(".welcome-user").html(`Welcome, ${traveler.name}`);
-
-  // let tripSummary = filterTrips.reduce((tripsSummary, trip) => {
-  //   return tripsSummary +=
-  //   `<section class="trip-card">
-  //   <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
-  //   <div><span>Trip Location:</span> ${trip.destination}</div>
-  //   <div><span>Trip date:</span> ${trip.date}</div>
-  //   <div><span>Trip Duration:</span> ${trip.duration}</div>
-  //   <div><span>Trip Status:</span> ${trip.status}</div>
-  //   <div>Overall Trip Cost: ${trip.overallCost}</div>
-  //   </section>
-  //   `
-  // },'')
-
-  let tripPresentSummary = filterPresentTrip.reduce((tripsSummary, trip) => {
-    return tripsSummary +=
-    `<section class="trip-card">
-    <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
-    <div><span>Trip Location:</span> ${trip.destination}</div>
-    <div><span>Trip date:</span> ${trip.date}</div>
-    <div><span>Trip Duration:</span> ${trip.duration}</div>
-    <div><span>Trip Status:</span> ${trip.status}</div>
-    <div>Overall Trip Cost: ${trip.overallCost}</div>
-    </section>
-    `
-  },'')
-
-  let pastTripSummary = filterPastTrips.reduce((tripsSummary, trip) => {
-    return tripsSummary +=
-    `<section class="trip-card">
-    <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
-    <div><span>Trip Location:</span> ${trip.destination}</div>
-    <div><span>Trip date:</span> ${trip.date}</div>
-    <div><span>Trip Duration:</span> ${trip.duration}</div>
-    <div><span>Trip Status:</span> ${trip.status}</div>
-    <div>Overall Trip Cost: ${trip.overallCost}</div>
-    </section>
-    `
-  },'')
-
-  let upcomingTrips = filterUpcomingTrips.reduce((tripsSummary, trip) => {
-    return tripsSummary +=
-    `<section class="trip-card">
-    <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
-    <div><span>Trip Location:</span> ${trip.destination}</div>
-    <div><span>Trip date:</span> ${trip.date}</div>
-    <div><span>Trip Duration:</span> ${trip.duration}</div>
-    <div><span>Trip Status:</span> ${trip.status}</div>
-    <div>Overall Trip Cost: ${trip.overallCost}</div>
-    </section>
-    `
-  },'')
-
-  let pendingTrips = filterPendingTrips.reduce((tripsSummary, trip) => {
-    return tripsSummary +=
-    `<section class="trip-card">
-    <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
-    <div><span>Trip Location:</span> ${trip.destination}</div>
-    <div><span>Trip date:</span> ${trip.date}</div>
-    <div><span>Trip Duration:</span> ${trip.duration}</div>
-    <div><span>Trip Status:</span> ${trip.status}</div>
-    <div>Overall Trip Cost: ${trip.overallCost}</div>
-    </section>
-    `
-  },'')
+  let presentTripsSummary = populateUserTrips(traveler.filterPresentTrips());
+  let pastTripsSummary = populateUserTrips(traveler.filterPastTrips());
+  let upcomingTrips = populateUserTrips(traveler.filterUpcomingTrips());
+  let pendingTrips = populateUserTrips(traveler.filterPendingTrips());
 
   return `
     <section class="user-summary">
       <h2>Traveler Summary</h2>
-      <div>Current Year Total Spent:</div>
-        <div>${traveler.calculateTotalSpentForYear(moment().format("YYYY"))}</div>
-      <div>All-Time Total Spent:</div>
-        <div>${traveler.calculateTotalSpentOverall()}</div>
+      <div>${moment().format("YYYY")} Spending</div>
+        <div>${formatter.format(traveler.calculateTotalSpentForYear(moment().format("YYYY")))}</div>
+      <div>All-Time Spending</div>
+        <div>${formatter.format(traveler.calculateTotalSpentOverall())}</div>
     </section>
     <section class="user-trip-summary">
       <h2>Past Trips</h2>
-      <div>${pastTripSummary}</div>
+      <div>${pastTripsSummary}</div>
     </section>
     <section class="user-trip-summary">
       <h2>Present Trips</h2>
-      <div>${tripPresentSummary}</div>
+      <div>${presentTripsSummary}</div>
     </section>
     <section class="user-trip-summary">
       <h2>Upcoming Trips</h2>
@@ -181,3 +109,23 @@ function populateTravelerInfo(userID, travelersData) {
       <div class="search-results"></div>
     </section>`
 }
+
+function populateUserTrips(tripsData) {
+  return tripsData.reduce((tripsSummary, trip) => {
+    return tripsSummary +=
+    `<section class="trip-card">
+      <img class="destination-image" src="${trip.destinationImage}" alt="${trip.destinationAlt}">
+      <div><span>Trip Location:</span> ${trip.destination}</div>
+      <div><span>Trip date:</span> ${trip.date}</div>
+      <div><span>Trip Duration:</span> ${trip.duration}</div>
+      <div><span>Trip Status:</span> ${trip.status}</div>
+      <div><span>Overall Trip Cost:</span> ${formatter.format(trip.overallCost)}</div>
+    </section>`
+  },'')
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0
+})
